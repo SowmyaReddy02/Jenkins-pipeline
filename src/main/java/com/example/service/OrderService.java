f6cc50bcfad0e4181a49d4d13defe9b928582e19
SowmyaReddy02 <127983629+SowmyaReddy02@users.noreply.github.com>
2024-07-25 12:27:51 -0400

    Update OrderService.java

--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -19,6 +19,10 @@ public class OrderService {
 		orderResponse.setId(order.getOrderId());
 		orderResponse.setName(order.getOrderName());
 		System.out.println("sysout");
+		System.out.println("one");
+		System.out.println("two");
+		System.out.println("three");
+		System.out.println("four");
 
 		return orderResponse;
 	}
